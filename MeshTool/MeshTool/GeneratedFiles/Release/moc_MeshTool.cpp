35f9c74d01695c263fe857657849253ff925c459
Tim Dörries <doerriet@hochschule-trier.de>
2018-08-22 04:48:02 +0200

    -removed unused files

--- MeshTool/MeshTool/GeneratedFiles/Release/moc_MeshTool.cpp
+++ MeshTool/MeshTool/GeneratedFiles/Release/moc_MeshTool.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MeshTool_t {
-    QByteArrayData data[25];
-    char stringdata0[697];
+    QByteArrayData data[34];
+    char stringdata0[1157];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -34,51 +34,70 @@ static const qt_meta_stringdata_MeshTool_t qt_meta_stringdata_MeshTool = {
 QT_MOC_LITERAL(0, 0, 8), // "MeshTool"
 QT_MOC_LITERAL(1, 9, 23), // "on_actionOpen_triggered"
 QT_MOC_LITERAL(2, 33, 0), // ""
-QT_MOC_LITERAL(3, 34, 31), // "on_actionCenterCamera_triggered"
-QT_MOC_LITERAL(4, 66, 32), // "on_actionToggleWireframe_toggled"
-QT_MOC_LITERAL(5, 99, 8), // "_enabled"
-QT_MOC_LITERAL(6, 108, 28), // "on_actionDefaultView_toggled"
-QT_MOC_LITERAL(7, 137, 27), // "on_actionRenderView_toggled"
-QT_MOC_LITERAL(8, 165, 23), // "on_actionUvView_toggled"
-QT_MOC_LITERAL(9, 189, 32), // "on_albedoRedSpinBox_valueChanged"
-QT_MOC_LITERAL(10, 222, 6), // "_value"
-QT_MOC_LITERAL(11, 229, 34), // "on_albedoGreenSpinBox_valueCh..."
-QT_MOC_LITERAL(12, 264, 33), // "on_albedoBlueSpinBox_valueCha..."
-QT_MOC_LITERAL(13, 298, 31), // "on_metallicSpinBox_valueChanged"
-QT_MOC_LITERAL(14, 330, 32), // "on_roughnessSpinBox_valueChanged"
-QT_MOC_LITERAL(15, 363, 34), // "on_emissiveRedSpinBox_valueCh..."
-QT_MOC_LITERAL(16, 398, 36), // "on_emissiveGreenSpinBox_value..."
-QT_MOC_LITERAL(17, 435, 35), // "on_emissiveBlueSpinBox_valueC..."
-QT_MOC_LITERAL(18, 471, 30), // "on_albedoTextureButton_clicked"
-QT_MOC_LITERAL(19, 502, 30), // "on_normalTextureButton_clicked"
-QT_MOC_LITERAL(20, 533, 32), // "on_metallicTextureButton_clicked"
-QT_MOC_LITERAL(21, 566, 33), // "on_roughnessTextureButton_cli..."
-QT_MOC_LITERAL(22, 600, 26), // "on_aoTextureButton_clicked"
-QT_MOC_LITERAL(23, 627, 32), // "on_emissiveTextureButton_clicked"
-QT_MOC_LITERAL(24, 660, 36) // "on_displacementTextureButton_..."
+QT_MOC_LITERAL(3, 34, 36), // "on_actionOpenAlbedoTexture_tr..."
+QT_MOC_LITERAL(4, 71, 38), // "on_actionOpenMetallicTexture_..."
+QT_MOC_LITERAL(5, 110, 39), // "on_actionOpenRoughnessTexture..."
+QT_MOC_LITERAL(6, 150, 46), // "on_actionOpenAmbientOcclusion..."
+QT_MOC_LITERAL(7, 197, 38), // "on_actionOpenEmissiveTexture_..."
+QT_MOC_LITERAL(8, 236, 42), // "on_actionOpenDisplacementText..."
+QT_MOC_LITERAL(9, 279, 36), // "on_actionSaveAlbedoTexture_tr..."
+QT_MOC_LITERAL(10, 316, 38), // "on_actionSaveMetallicTexture_..."
+QT_MOC_LITERAL(11, 355, 39), // "on_actionSaveRoughnessTexture..."
+QT_MOC_LITERAL(12, 395, 46), // "on_actionSaveAmbientOcclusion..."
+QT_MOC_LITERAL(13, 442, 38), // "on_actionSaveEmissiveTexture_..."
+QT_MOC_LITERAL(14, 481, 42), // "on_actionSaveDisplacementText..."
+QT_MOC_LITERAL(15, 524, 34), // "on_actionSaveAllTextures_trig..."
+QT_MOC_LITERAL(16, 559, 38), // "on_actionActivateAlbedoTextur..."
+QT_MOC_LITERAL(17, 598, 8), // "_enabled"
+QT_MOC_LITERAL(18, 607, 40), // "on_actionActivateMetallicText..."
+QT_MOC_LITERAL(19, 648, 41), // "on_actionActivateRoughnessTex..."
+QT_MOC_LITERAL(20, 690, 48), // "on_actionActivateAmbientOcclu..."
+QT_MOC_LITERAL(21, 739, 40), // "on_actionActivateEmissiveText..."
+QT_MOC_LITERAL(22, 780, 44), // "on_actionActivateDisplacement..."
+QT_MOC_LITERAL(23, 825, 33), // "on_actionSetStrokeWidth_trigg..."
+QT_MOC_LITERAL(24, 859, 27), // "on_actionSetPaint_triggered"
+QT_MOC_LITERAL(25, 887, 30), // "on_actionClearActive_triggered"
+QT_MOC_LITERAL(26, 918, 27), // "on_actionClearAll_triggered"
+QT_MOC_LITERAL(27, 946, 35), // "on_actionSetEmissiveColor_tri..."
+QT_MOC_LITERAL(28, 982, 31), // "on_actionCenterCamera_triggered"
+QT_MOC_LITERAL(29, 1014, 32), // "on_actionToggleWireframe_toggled"
+QT_MOC_LITERAL(30, 1047, 28), // "on_actionDefaultView_toggled"
+QT_MOC_LITERAL(31, 1076, 28), // "on_actionTextureView_toggled"
+QT_MOC_LITERAL(32, 1105, 27), // "on_actionRenderView_toggled"
+QT_MOC_LITERAL(33, 1133, 23) // "on_actionUvView_toggled"
 
     },
     "MeshTool\0on_actionOpen_triggered\0\0"
+    "on_actionOpenAlbedoTexture_triggered\0"
+    "on_actionOpenMetallicTexture_triggered\0"
+    "on_actionOpenRoughnessTexture_triggered\0"
+    "on_actionOpenAmbientOcclusionTexture_triggered\0"
+    "on_actionOpenEmissiveTexture_triggered\0"
+    "on_actionOpenDisplacementTexture_triggered\0"
+    "on_actionSaveAlbedoTexture_triggered\0"
+    "on_actionSaveMetallicTexture_triggered\0"
+    "on_actionSaveRoughnessTexture_triggered\0"
+    "on_actionSaveAmbientOcclusionTexture_triggered\0"
+    "on_actionSaveEmissiveTexture_triggered\0"
+    "on_actionSaveDisplacementTexture_triggered\0"
+    "on_actionSaveAllTextures_triggered\0"
+    "on_actionActivateAlbedoTexture_toggled\0"
+    "_enabled\0on_actionActivateMetallicTexture_toggled\0"
+    "on_actionActivateRoughnessTexture_toggled\0"
+    "on_actionActivateAmbientOcclusionTexture_toggled\0"
+    "on_actionActivateEmissiveTexture_toggled\0"
+    "on_actionActivateDisplacementTexture_toggled\0"
+    "on_actionSetStrokeWidth_triggered\0"
+    "on_actionSetPaint_triggered\0"
+    "on_actionClearActive_triggered\0"
+    "on_actionClearAll_triggered\0"
+    "on_actionSetEmissiveColor_triggered\0"
     "on_actionCenterCamera_triggered\0"
     "on_actionToggleWireframe_toggled\0"
-    "_enabled\0on_actionDefaultView_toggled\0"
+    "on_actionDefaultView_toggled\0"
+    "on_actionTextureView_toggled\0"
     "on_actionRenderView_toggled\0"
-    "on_actionUvView_toggled\0"
-    "on_albedoRedSpinBox_valueChanged\0"
-    "_value\0on_albedoGreenSpinBox_valueChanged\0"
-    "on_albedoBlueSpinBox_valueChanged\0"
-    "on_metallicSpinBox_valueChanged\0"
-    "on_roughnessSpinBox_valueChanged\0"
-    "on_emissiveRedSpinBox_valueChanged\0"
-    "on_emissiveGreenSpinBox_valueChanged\0"
-    "on_emissiveBlueSpinBox_valueChanged\0"
-    "on_albedoTextureButton_clicked\0"
-    "on_normalTextureButton_clicked\0"
-    "on_metallicTextureButton_clicked\0"
-    "on_roughnessTextureButton_clicked\0"
-    "on_aoTextureButton_clicked\0"
-    "on_emissiveTextureButton_clicked\0"
-    "on_displacementTextureButton_clicked"
+    "on_actionUvView_toggled"
 };
 #undef QT_MOC_LITERAL
 
@@ -88,7 +107,7 @@ static const uint qt_meta_data_MeshTool[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      21,   14, // methods
+      31,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -96,43 +115,41 @@ static const uint qt_meta_data_MeshTool[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,  119,    2, 0x08 /* Private */,
-       3,    0,  120,    2, 0x08 /* Private */,
-       4,    1,  121,    2, 0x08 /* Private */,
-       6,    1,  124,    2, 0x08 /* Private */,
-       7,    1,  127,    2, 0x08 /* Private */,
-       8,    1,  130,    2, 0x08 /* Private */,
-       9,    1,  133,    2, 0x08 /* Private */,
-      11,    1,  136,    2, 0x08 /* Private */,
-      12,    1,  139,    2, 0x08 /* Private */,
-      13,    1,  142,    2, 0x08 /* Private */,
-      14,    1,  145,    2, 0x08 /* Private */,
-      15,    1,  148,    2, 0x08 /* Private */,
-      16,    1,  151,    2, 0x08 /* Private */,
-      17,    1,  154,    2, 0x08 /* Private */,
-      18,    0,  157,    2, 0x08 /* Private */,
-      19,    0,  158,    2, 0x08 /* Private */,
-      20,    0,  159,    2, 0x08 /* Private */,
-      21,    0,  160,    2, 0x08 /* Private */,
-      22,    0,  161,    2, 0x08 /* Private */,
-      23,    0,  162,    2, 0x08 /* Private */,
-      24,    0,  163,    2, 0x08 /* Private */,
+       1,    0,  169,    2, 0x08 /* Private */,
+       3,    0,  170,    2, 0x08 /* Private */,
+       4,    0,  171,    2, 0x08 /* Private */,
+       5,    0,  172,    2, 0x08 /* Private */,
+       6,    0,  173,    2, 0x08 /* Private */,
+       7,    0,  174,    2, 0x08 /* Private */,
+       8,    0,  175,    2, 0x08 /* Private */,
+       9,    0,  176,    2, 0x08 /* Private */,
+      10,    0,  177,    2, 0x08 /* Private */,
+      11,    0,  178,    2, 0x08 /* Private */,
+      12,    0,  179,    2, 0x08 /* Private */,
+      13,    0,  180,    2, 0x08 /* Private */,
+      14,    0,  181,    2, 0x08 /* Private */,
+      15,    0,  182,    2, 0x08 /* Private */,
+      16,    1,  183,    2, 0x08 /* Private */,
+      18,    1,  186,    2, 0x08 /* Private */,
+      19,    1,  189,    2, 0x08 /* Private */,
+      20,    1,  192,    2, 0x08 /* Private */,
+      21,    1,  195,    2, 0x08 /* Private */,
+      22,    1,  198,    2, 0x08 /* Private */,
+      23,    0,  201,    2, 0x08 /* Private */,
+      24,    0,  202,    2, 0x08 /* Private */,
+      25,    0,  203,    2, 0x08 /* Private */,
+      26,    0,  204,    2, 0x08 /* Private */,
+      27,    0,  205,    2, 0x08 /* Private */,
+      28,    0,  206,    2, 0x08 /* Private */,
+      29,    1,  207,    2, 0x08 /* Private */,
+      30,    1,  210,    2, 0x08 /* Private */,
+      31,    1,  213,    2, 0x08 /* Private */,
+      32,    1,  216,    2, 0x08 /* Private */,
+      33,    1,  219,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::Bool,    5,
-    QMetaType::Void, QMetaType::Bool,    5,
-    QMetaType::Void, QMetaType::Bool,    5,
-    QMetaType::Void, QMetaType::Bool,    5,
-    QMetaType::Void, QMetaType::Double,   10,
-    QMetaType::Void, QMetaType::Double,   10,
-    QMetaType::Void, QMetaType::Double,   10,
-    QMetaType::Void, QMetaType::Double,   10,
-    QMetaType::Void, QMetaType::Double,   10,
-    QMetaType::Void, QMetaType::Double,   10,
-    QMetaType::Void, QMetaType::Double,   10,
-    QMetaType::Void, QMetaType::Double,   10,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -140,6 +157,28 @@ static const uint qt_meta_data_MeshTool[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, QMetaType::Bool,   17,
 
        0        // eod
 };
@@ -151,26 +190,36 @@ void MeshTool::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, vo
         Q_UNUSED(_t)
         switch (_id) {
         case 0: _t->on_actionOpen_triggered(); break;
-        case 1: _t->on_actionCenterCamera_triggered(); break;
-        case 2: _t->on_actionToggleWireframe_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
-        case 3: _t->on_actionDefaultView_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
-        case 4: _t->on_actionRenderView_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
-        case 5: _t->on_actionUvView_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
-        case 6: _t->on_albedoRedSpinBox_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 7: _t->on_albedoGreenSpinBox_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 8: _t->on_albedoBlueSpinBox_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 9: _t->on_metallicSpinBox_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 10: _t->on_roughnessSpinBox_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 11: _t->on_emissiveRedSpinBox_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 12: _t->on_emissiveGreenSpinBox_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 13: _t->on_emissiveBlueSpinBox_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 14: _t->on_albedoTextureButton_clicked(); break;
-        case 15: _t->on_normalTextureButton_clicked(); break;
-        case 16: _t->on_metallicTextureButton_clicked(); break;
-        case 17: _t->on_roughnessTextureButton_clicked(); break;
-        case 18: _t->on_aoTextureButton_clicked(); break;
-        case 19: _t->on_emissiveTextureButton_clicked(); break;
-        case 20: _t->on_displacementTextureButton_clicked(); break;
+        case 1: _t->on_actionOpenAlbedoTexture_triggered(); break;
+        case 2: _t->on_actionOpenMetallicTexture_triggered(); break;
+        case 3: _t->on_actionOpenRoughnessTexture_triggered(); break;
+        case 4: _t->on_actionOpenAmbientOcclusionTexture_triggered(); break;
+        case 5: _t->on_actionOpenEmissiveTexture_triggered(); break;
+        case 6: _t->on_actionOpenDisplacementTexture_triggered(); break;
+        case 7: _t->on_actionSaveAlbedoTexture_triggered(); break;
+        case 8: _t->on_actionSaveMetallicTexture_triggered(); break;
+        case 9: _t->on_actionSaveRoughnessTexture_triggered(); break;
+        case 10: _t->on_actionSaveAmbientOcclusionTexture_triggered(); break;
+        case 11: _t->on_actionSaveEmissiveTexture_triggered(); break;
+        case 12: _t->on_actionSaveDisplacementTexture_triggered(); break;
+        case 13: _t->on_actionSaveAllTextures_triggered(); break;
+        case 14: _t->on_actionActivateAlbedoTexture_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 15: _t->on_actionActivateMetallicTexture_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 16: _t->on_actionActivateRoughnessTexture_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 17: _t->on_actionActivateAmbientOcclusionTexture_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 18: _t->on_actionActivateEmissiveTexture_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 19: _t->on_actionActivateDisplacementTexture_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 20: _t->on_actionSetStrokeWidth_triggered(); break;
+        case 21: _t->on_actionSetPaint_triggered(); break;
+        case 22: _t->on_actionClearActive_triggered(); break;
+        case 23: _t->on_actionClearAll_triggered(); break;
+        case 24: _t->on_actionSetEmissiveColor_triggered(); break;
+        case 25: _t->on_actionCenterCamera_triggered(); break;
+        case 26: _t->on_actionToggleWireframe_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 27: _t->on_actionDefaultView_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 28: _t->on_actionTextureView_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 29: _t->on_actionRenderView_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 30: _t->on_actionUvView_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
         default: ;
         }
     }
@@ -203,13 +252,13 @@ int MeshTool::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 21)
+        if (_id < 31)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 21;
+        _id -= 31;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 21)
+        if (_id < 31)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 21;
+        _id -= 31;
     }
     return _id;
 }
